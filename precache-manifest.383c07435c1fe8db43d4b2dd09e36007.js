abbee6dbf25630be014777c903757bfbe791ecf8
Andrew Zacharias <andyzack@gmail.com>
2020-02-11 18:48:28 +1100

    Updates

---
+++ precache-manifest.383c07435c1fe8db43d4b2dd09e36007.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d31c6514ece4d8b7f996bd9cade6ce",
+    "url": "/book-explorer/index.html"
+  },
+  {
+    "revision": "7219216c1778f939a15d",
+    "url": "/book-explorer/static/css/main.ee89dd8a.chunk.css"
+  },
+  {
+    "revision": "064f2697688c18ea897f",
+    "url": "/book-explorer/static/js/2.4d0668db.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/book-explorer/static/js/2.4d0668db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7219216c1778f939a15d",
+    "url": "/book-explorer/static/js/main.8ddf8a2e.chunk.js"
+  },
+  {
+    "revision": "442d4f28ab5235a55c7a",
+    "url": "/book-explorer/static/js/runtime-main.38087890.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/book-explorer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "773d1a1cf1a8133ed04712716d933c71",
+    "url": "/book-explorer/static/media/picture-1.773d1a1c.svg"
+  }
+]);
